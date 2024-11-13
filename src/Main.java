f75621ace860cf5b7ee1c707f16f513db104157e
tomasc <tomascuts@gmail.com>
2024-11-12 23:11:38 -0300

    Limpieza de main para evitar errores

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,7 @@ import entities.Credencial;
 
 public class Main {
     public static void main(String[] args) {
-        mostrarAccesos();
+
     }
 
-
-
-    public static void mostrarAccesos(){
-        Usuario ana = new Usuario("Ana", 19, 27889345, new Credencial(1));
-        Usuario luis = new Usuario("Luis",15, 29669784,  new Credencial(2));
-
-        Hotel hotel = new Hotel();
-
-        hotel.mostrarAccesos(ana);
-        hotel.mostrarAccesos(luis);
-    }
 }
